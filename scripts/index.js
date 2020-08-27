7806616ad0ee07516c864e9e55fbe87a5de7b51b
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-08-27 14:45:17 -0400

    Fixed Dropdown button; Cleaned the Javascript code

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,5 @@ let navButton = document.getElementsByClassName("nav-button");
 let toggle = false;
 let navList = document.getElementsByClassName("nav-list");
 const displayDropdown = () => {
-    toggle = !toggle;
-    if (toggle) {
-        navList[0].classList.add("nav-button-toggled");
-    }
-    else {
-        navList[0].classList.remove("nav-button-toggled");
-    }
+    navList[0].classList.toggle("nav-list-toggled");
 }
