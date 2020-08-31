fab2c2aa2bb3a2ff6df7f3674aa7dc625f21a3fb
Kenneth Lu Diaz <ludiazken@gmail.com>
2020-08-31 10:21:51 -0400

    Fixed resizing of waterfall picture

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,16 @@ let navList = document.getElementsByClassName("nav-list");
 const displayDropdown = () => {
     navList[0].classList.toggle("nav-list-toggled");
 }
+
+
+//========Xtra========
+function changePic() {
+    let picture = document.getElementsByClassName('extra-info-picture');
+    if (window.screen.width <= 800) {
+        picture[0].src = "http://localhost:5500/images/waterfall700w.jpg";
+    }
+    else {
+        picture[0].src = "http://localhost:5500/images/waterfall.jpg";
+    }
+
+}
